b1be45dd7c0c4beb6da1277294f93b769cad544f
cocallaw <11371083+cocallaw@users.noreply.github.com>
2025-04-23 17:28:25 +0000

    Update dsc-cli-preview to 3.1.0-preview.3

--- Formula/dsc-cli-preview.rb
+++ Formula/dsc-cli-preview.rb
@@ -6,10 +6,10 @@ class DscCliPreview < Formula
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/PowerShell/DSC/releases/download/v3.1.0-preview.3/DSC-3.1.0-preview.3-aarch64-apple-darwin.tar.gz"
-        sha256 "PUT_ARM_SHA256_HERE"
+        sha256 "1b77bd224bf10316e995b37ba6bff6fc4705c557692439e77283d04b005735bb"
       else
         url "https://github.com/PowerShell/DSC/releases/download/v3.1.0-preview.3/DSC-3.1.0-preview.3-x86_64-apple-darwin.tar.gz"
-        sha256 "PUT_INTEL_SHA256_HERE"
+        sha256 "1b77bd224bf10316e995b37ba6bff6fc4705c557692439e77283d04b005735bb"
       end
     end
   
